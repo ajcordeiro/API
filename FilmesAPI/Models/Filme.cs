d8d1722a8c12658354b71951c17afd5b8dbac3ac
Jaqueson Cordeiro Alves <p22.jaqueson@webmotors.com.br>
2023-03-02 18:23:20 -0300

    Add project files.

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage ="O Título do filme é obrigatório")]
+    public string Titulo { get; set; }
+
+    [Required]
+    [Range(70,600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+
+    [Required(ErrorMessage ="O Diretor é obrigatório")]
+    public string Diretor { get; set; }
+
+    [Required(ErrorMessage = "O Gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O Tamanho do gênero não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+}
